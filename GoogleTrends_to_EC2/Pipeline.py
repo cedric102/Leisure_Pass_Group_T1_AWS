86fcc937c9bfd2b6565e7820ae373347e585d139
cedric102 <carteroncedric@gmail.com>
2021-04-26 02:47:16 +0100

    Update Pipeline.py
    Remove the unnecessary code and comments.
    Consider using an asynchronous method to add the jobs so that it runs in a different thread and does not disrupt the flow for the rest of the program if needed.

--- GoogleTrends_to_EC2/Pipeline.py
+++ GoogleTrends_to_EC2/Pipeline.py
@@ -13,11 +13,7 @@ kw_list = ["Elon Musk" ,
         "Godzilla" ,
         "Black Panther"]
 
-
-# pytrends.build_payload( ["Godzilla"] , cat='0', timeframe='today 5-y' , geo='' , gprop='' )
-
 def check_trends():
-#    print(keyword)
     pytrends.build_payload( kw_list ,
                             cat='0' ,
                             timeframe='now 1-H' ,
@@ -26,28 +22,15 @@ def check_trends():
 
     data = pytrends.interest_over_time()
 
-    print( data.iloc[0] )
-#    dataset = []
     dataset.append(data)
     result = pd.concat(dataset, axis=0)
 
     with open('trends.csv' , 'a') as fd :
         fd.write(result.to_csv())
-#    result.to_csv('trends.csv')
-
-#    for i in data :
-#        for j in i :
-#            print( j )
     print( data )
 
-# for kw in kw_list :
-# keyword.append(kw)
 check_trends()
+
 scheduler = BlockingScheduler()
 scheduler.add_job( check_trends , 'interval' , hours = 1 )
 scheduler.start()
-check_trends()
-# keyword.clear()
-
-# aws s3 cp ~/june2018/results s3://myBucket/june2018/results --recursive --endpoint arn:aws:s3:eu-west-2:384962687271:accesspoint/accesspoint
-
